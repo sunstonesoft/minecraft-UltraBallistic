0f3efe78a45c19e18eead2549cb7a0efffb03a93
arkrafter <thearkrafter@gmail.com>
2023-02-13 22:58:43 +0200

    v2

--- UltraBallistic/src/main/java/com/sunstonesoft/mc/UltraBallistic/main.java
+++ UltraBallistic/src/main/java/com/sunstonesoft/mc/UltraBallistic/main.java
@@ -5,6 +5,13 @@ package com.sunstonesoft.mc.UltraBallistic;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.Resident;
+import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.Nation;
+
+import java.util.Random;
+
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.World;
@@ -12,6 +19,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 
@@ -31,6 +39,9 @@ public final class main extends JavaPlugin {
         getLogger().info("Ballistic missiles are ready!");
     }
 	
+	
+	
+	
 	private boolean setupEconomy() {
         if (getServer().getPluginManager().getPlugin("Vault") == null) {
             return false;
@@ -43,6 +54,35 @@ public final class main extends JavaPlugin {
         return econ != null;
     }
 	
+	public Boolean launchRocket(Location startloc, World world, Player player, int id) {
+
+		int x = (int) getConfig().get(String.format("inflight.%s.loc.x", id));
+		int y = (int) getConfig().get(String.format("inflight.%s.loc.y", id));
+		int z = (int) getConfig().get(String.format("inflight.%s.loc.z", id));
+		Location loc = new Location(world, x, y ,z);
+		Location landLoc = new Location(world, x, world.getHighestBlockYAt(startloc) ,z);
+		
+		world.spawnParticle(Particle.SMOKE_NORMAL, loc, 15);
+		
+		if (loc.getY() <= landLoc.getY()) {
+			world.createExplosion(landLoc, 15, true);
+			world.createExplosion(landLoc, 15, true);
+			world.createExplosion(landLoc, 15, true);
+			world.createExplosion(landLoc, 15, true);
+			world.createExplosion(landLoc, 15, true);
+			world.createExplosion(landLoc, 15, true);
+			world.createExplosion(landLoc, 15, true);
+			player.sendMessage("Rocket blow up!");
+			return true;
+		}
+		else {
+			getConfig().set(String.format("inflight.%s.loc.y",id), y - 1);
+			saveConfig();
+			return false;
+		}
+		
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("rockets")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
@@ -50,7 +90,7 @@ public final class main extends JavaPlugin {
 				sender.sendMessage("This command can only be run by a player.");
 			} else {
 				Player player = (Player) sender;
-				int rockets = (int) getConfig().get(String.format("rockets.%s", player.getUniqueId().toString()), 0);
+				int rockets = (int) getConfig().get(String.format("rockets.%s.ballistic", player.getUniqueId().toString()), 0);
 				
 				sender.sendMessage(String.format("You have %s rockets!", rockets));
 			}
@@ -61,18 +101,36 @@ public final class main extends JavaPlugin {
 				sender.sendMessage("This command can only be run by a player.");
 			} else {
 				Player player = (Player) sender;
+//				Resident resident = new Resident(player.getName());
+//				if (resident.hasTown()){
+//				    try {
+//						Town town = resident.getTown();
+//						Nation nation = town.getNationOrNull();
+//						if (!(nation==null)) {
+//							if (nation.getKing() == resident) {
+//								// making shit with declaring war
+//								
+//							}
+//						}
+//					} catch (NotRegisteredException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
 				
-				EconomyResponse r = econ.withdrawPlayer(player, (double) getConfig().get("cost",100000.00));
+				
+				
+				EconomyResponse r = econ.withdrawPlayer(player, (double) getConfig().get("cost.ballistic",100000.00));
 						
 				if(r.transactionSuccess()) {
 					
-					int rockets = (int) getConfig().get(String.format("rockets.%s", player.getUniqueId().toString()), 0);
+					int rockets = (int) getConfig().get(String.format("rockets.%s.ballistic", player.getUniqueId().toString()), 0);
 					
 					rockets = rockets + 1;
 					
 					
 					
-					getConfig().set(String.format("rockets.%s", player.getUniqueId().toString()), rockets);
+					getConfig().set(String.format("rockets.%s.ballistic", player.getUniqueId().toString()), rockets);
 					saveConfig();
 					player.sendMessage(String.format("You succesfully bought 1 rocket. Now you have %s rockets and your balance is %s!", rockets, econ.format(r.balance)));
 				}
@@ -85,11 +143,14 @@ public final class main extends JavaPlugin {
 			}
 			return true;
 		}
+
+		
+		
 		if (cmd.getName().equalsIgnoreCase("launch_rocket")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
 			if (!(sender instanceof Player)) {
 				sender.sendMessage("This command can only be run by a player.");
 			} else {
-				Player player = (Player) sender;
+				final Player player = (Player) sender;
 				
 				if (args.length > 2) {
 			        sender.sendMessage("Too many arguments!");
@@ -101,7 +162,7 @@ public final class main extends JavaPlugin {
 			    }
 			    
 			    
-				int rockets = (int) getConfig().get(String.format("rockets.%s", player.getUniqueId().toString()), 0);
+				int rockets = (int) getConfig().get(String.format("rockets.%s.ballistic", player.getUniqueId().toString()), 0);
 				
 				if (rockets < 1) {
 					player.sendMessage("You don't have enough rockets!");
@@ -110,52 +171,36 @@ public final class main extends JavaPlugin {
 				
 				rockets = rockets - 1;
 				
-				getConfig().set(String.format("rockets.%s", player.getUniqueId().toString()), rockets);
+				getConfig().set(String.format("rockets.%s.ballistic", player.getUniqueId().toString()), rockets);
 				saveConfig();
 				
-						
-
-				try {
-					
-					World world = player.getWorld();
-					int x = Integer.parseInt(args[0]);
-					int z = Integer.parseInt(args[1]);
-			        int y = world.getHighestBlockAt(x, z).getY();
-			        
-			        
-			        
-					Location loc = new Location(world, x, y, z);
-					
-					int cycleY = 255;
-					
-					while (cycleY > loc.getY()) {
-						
-						
-						
-						cycleY = cycleY - 1;
-						Location cycleLoc = new Location(world, x, cycleY, z);
-						world.spawnParticle(Particle.SMOKE_NORMAL, cycleLoc, 2);
-					}
-					
-					if (cycleY == loc.getY()) {
-						world.createExplosion(loc, 15, true);
-						world.createExplosion(loc, 15, true);
-						world.createExplosion(loc, 15, true);
-						world.createExplosion(loc, 15, true);
-						world.createExplosion(loc, 15, true);
-						world.createExplosion(loc, 15, true);
-						world.createExplosion(loc, 15, true);
-						player.sendMessage("Rocket blow up!");
-						return true;
-					}
-					
-					
-							
-							
-			    } catch (final NumberFormatException e) {
-			        return false;
-			    }
 				
+				int x = Integer.parseInt(args[0]);
+				int z = Integer.parseInt(args[1]);
+				final World world = player.getWorld();
+		        int y = 255;
+				final Location loc = new Location(world, x, y, z);
+				Random rand = new Random();
+				final int id = rand.nextInt(500000000);
+				
+				getConfig().set(String.format("inflight.%s.id", id), id);
+				getConfig().set(String.format("inflight.%s.loc.x", id), x);
+				getConfig().set(String.format("inflight.%s.loc.y", id), y);
+				getConfig().set(String.format("inflight.%s.loc.z", id), z);
+				saveConfig();
+				
+				new BukkitRunnable() {
+				    @Override
+				    public void run() {
+						Boolean ret = launchRocket(loc, world, player, id);
+						if (ret == true) {
+							getConfig().set(String.format("inflight.%s", id), null);
+							cancel();
+						}
+				    }
+				}.runTaskTimer(this, 0, 5);
+
+				return true;
 				
 			}
 			return true;
